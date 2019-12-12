5a3202aaf92843ed8c6fa8574a7fdabb7e20c7fe
gracebuchardt <43751546+gracebuchardt@users.noreply.github.com>
2019-12-11 21:16:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HeatNeededCalc
+ calculates amount of heat needed to turn water(solid/liquid/gas?) to another temperature (solid/liquid/gas?), in joules.
---
+++ src/MainCalc.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class MainCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//GOAL: to enter in initial heat, and desired final heat, and
+		//the amount of heat required for said action will be printed out.
+		double mass =0;
+		double Ti =0;
+		double Tf =0;
+	    double statei =0;
+	    double statef =0;
+	    double Q;
+	    double 
+		
+		System.out.println("Water heat Calculator:");
+		System.out.println("By entering a mass, initial heat, and desired heat,");
+		System.out.println("the amount of heat (in joules) need to");
+		System.out.println("reach said temp will be calculated.");
+		
+		Scanner in=new Scanner(System.in);
+		
+		System.out.println("  ");
+		System.out.println("Enter mass?");
+		mass=in.nextDouble();
+		
+		System.out.println(" ");
+		System.out.println("Enter starting temperature?");
+		Ti=in.nextDouble();
+		
+		System.out.println(" ");
+		System.out.println("Enter desired temperature?");
+		Tf=in.nextDouble();
+		
+		if (Ti<0);
+		//if it is less than 0, it is a solid. 
+		
+		// Q=MCdeltaT
+		// energy (in kJ) = mass, heatcapacity, temp. 
+		
+		
+		
+		
+		
+	}
+
+}
